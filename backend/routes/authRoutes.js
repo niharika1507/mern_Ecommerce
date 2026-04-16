cac108a69480647b174b047d0b9e11599af77227
niharika1507 <nndniharika@gmail.com>
2026-04-16 16:23:16 +0530

    day 2 admin, add product

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,17 +2,18 @@ const express= require("express")
 const router=express.Router() 
 const bcrypt=require("bcrypt")
 const jwt= require("jsonwebtoken")
-const User= require("../models/user")
+const User= require("../models/User")
 
 router.post("/register",async (req,res)=>{
     try{
         const {name,email,password,mobile}=req.body 
+        console.log("----------",name,email,password,mobile)
         const existingUser= await User.findOne({email})
 
         if(existingUser){
             return res.status(401).json({message:"User already exists"})
         }
-        const hashedPassword= await bcrypt.hash(password,10)
+        const hashedPassword=await bcrypt.hash(password,10)
         const user=await User.create({
             name,email,password:hashedPassword,mobile
         })
@@ -23,6 +24,31 @@ router.post("/register",async (req,res)=>{
         return res.status(500).json({message:"Error in server"})
     }
 })
+
+router.post("/login",async (req,res)=>{
+    try{
+        const {email,password}=req.body 
+        let user=await User.findOne({email})
+        if(!user){
+            return res.status(401).json({message:"User email not found"})
+        }
+        let isMatch=bcrypt.compare(password,user.Password)
+        if(!isMatch){
+            return res.status(401).json({message:"Invalid password"})
+        }
+        const token=jwt.sign(
+            {id:user._id,role:user.role},
+            process.env.JWT_SECRET,
+            {expiresIn:"1d"}
+        )
+        return res.status(200).json({message:"Login successful"})
+    }
+    catch(err){
+        console.log("from login route",err)
+        return res.status(500).json({message:`from login route server error ${err}`})
+    }
+})
+
 
 
 module.exports=router
--- client/src/components/register.jsx
+++ client/src/components/register.jsx
@@ -1,58 +1,73 @@
-
-import React from 'react'
+import React,{useState} from 'react'
 import API from '../api/axios'
 export default function Register() {
-    const [name, setName] = React.useState("")
-    const [email, setEmail] = React.useState("")
-    const [password, setPassword] = React.useState("")
-    const [mobile, setMobile] = React.useState("")
-    function handleRegister(e) {
+    const [name,setName]=useState("")
+    const [email,setEmail]=useState("")
+    const [password,setPassword]=useState("")
+    const [mobile,setMobile]=useState("")
+    function handleRegister(e){
         e.preventDefault()
-        API.post('/register', { name, email, password, mobile:Number })
-
-            .then((res) => {
-                console.log(res.data)
-                if(res.status === 201){
-                    alert("User created successfully")
+       
+        API.post("/auth/register",{name,email,password,mobile:Number(mobile)})
+            .then((res)=>{
+                console.log("from then method",res)
+                if(res.status===201){
+                    alert("Registration successfully")
+                }
+                else if(res.status==401){
+                    alert(res.data.message)
                 }
             })
-            .catch(err => console.log(err))
+            .catch(err=>{
+                console.log("from catch block",err)
+                if(err.status==401){
+                    alert(err.response.data.message)
+                }
+            })
     }
-  return (
-    <div className ='container'>
-        <div className='row'>
-            <form className='col-12 col-md-6'>
-                <div className = 'mb-3'>
-                <h1>Register</h1>
-                </div>
-                <div className = 'mb-3'>
-                    <label htmlFor='name' className='form-label'>Name</label>
-                    <input type='text' 
-                    className="form-control"
-                    onChange={(e)=>setName(e.target.value)}/>
-                </div>
-                <div className = 'mb-3'>
-                    <label htmlFor='email' className='form-label'>Email</label>
-                    <input type='email' 
-                    className="form-control"
-                    onChange={(e)=>setEmail(e.target.value)}/>
-                </div>
-                <div className = 'mb-3'>
-                    <label htmlFor='password' className='form-label'>Password</label>
-                    <input type='password' 
-                    className="form-control"
-                    onChange={(e)=>setPassword(e.target.value)}/>
-                </div>
-                <div className = 'mb-3'>
-                    <label htmlFor='mobile' className='form-label'>Mobile</label>
-                    <input type='text' 
-                    className="form-control"
-                    onChange={(e)=>setMobile(e.target.value)}/>
-                </div>
-                <button className='btn btn-success'>Register</button>
-            </form>
+    return (
+        <div className='container'>
+            <div className='row'>
+                <form onSubmit={handleRegister} className='col-12 col-md-6'>
+                    <div className='mb-3'>
+                        <h1>Register</h1>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="" className="form-label">Name</label>
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            name="name"
+                            onChange={(e)=>setName(e.target.value)}/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="" className="form-label">Email</label>
+                        <input 
+                            type="email" 
+                            className="form-control" 
+                            name="email"
+                            onChange={(e)=>setEmail(e.target.value)}/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="" className="form-label">Password</label>
+                        <input 
+                            type="password" 
+                            className="form-control" 
+                            name="password"
+                            onChange={(e)=>setPassword(e.target.value)}/>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="" className="form-label">Mobile Number</label>
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            name="mobile"
+                            onChange={(e)=>setMobile(e.target.value)}/>
+                    </div>
+                    <button className='btn btn-success'>Register</button>
+                </form>
+            </div>
+
         </div>
-    </div>
-    
-  )
+    )
 }
